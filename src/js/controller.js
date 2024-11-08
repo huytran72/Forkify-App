6e96e375827cb75c2265011cbe18a9e057d9ffe3
Huy Tran <baohuy82.th@gmail.com>
2024-11-08 10:58:06 -0800

    Update

--- src/js/controller.js
+++ src/js/controller.js
@@ -18,6 +18,8 @@ const showRecipe = async function () {
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
     const data = await res.json();
+
+    console.log(res, data);
   } catch (err) {
     alert(err);
   }
