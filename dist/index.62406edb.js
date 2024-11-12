431cef234bd38307e1ff2fa7c9148d7f82746407
Huy Tran <baohuy82.th@gmail.com>
2024-11-11 21:25:05 -0800

    update

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -10,6 +10,7 @@ const timeout = function(s) {
 ///////////////////////////////////////
 const showRecipe = async function() {
     try {
+        // 1) Loading recipe
         const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
         const data = await res.json();
         if (!res.ok) throw new Error(`${data.message} (${res.status})`);
--- src/js/controller.js
+++ src/js/controller.js
@@ -14,6 +14,7 @@ const timeout = function (s) {
 
 const showRecipe = async function () {
   try {
+    // 1) Loading recipe
     const res = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca5a'
