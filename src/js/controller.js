3e387235f82c98f4f355ea2e3e6ca9b9ee4a5adb
Huy Tran <baohuy82.th@gmail.com>
2024-11-11 21:26:22 -0800

    Update

--- src/js/controller.js
+++ src/js/controller.js
@@ -38,6 +38,69 @@ const showRecipe = async function () {
     console.log(recipe);
 
     // 2) Rendering recipe
+    const markup = `
+      <figure class="recipe__fig">
+        <img src="${recipe.image}" alt="${
+      recipe.title
+    }" class="recipe__img" />
+        <h1 class="recipe__title">
+          <span>${recipe.title}</span>
+        </h1>
+      </figure>
+
+      <div class="recipe__details">
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="src/img/icons.svg#icon-clock"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--minutes">${
+            recipe.cookingTime
+          }</span>
+          <span class="recipe__info-text">minutes</span>
+        </div>
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="src/img/icons.svg#icon-users"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--people">${
+            recipe.servings
+          }</span>
+          <span class="recipe__info-text">servings</span>
+
+          <div class="recipe__info-buttons">
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="src/img/icons.svg#icon-minus-circle"></use>
+              </svg>
+            </button>
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="src/img/icons.svg#icon-plus-circle"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <button class="btn--round">
+          <svg class="">
+            <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+          </svg>
+        </button>
+      </div>
+
+      <div class="recipe__ingredients">
+        <h2 class="heading--2">Recipe ingredients</h2>
+        <ul class="recipe__ingredient-list">
+          ${recipe.ingredients
+            .map(ing => {
+              return `
+              <li class="recipe__ingredient">
+                <svg class="recipe__icon">
+                  <use href="src/img/icons.svg#icon-check"></use>
+                </svg>
+                <div class="recipe__quantity">${ing.quantity}</div>
+                <div class="recipe__description">
+                  <span class="recipe__unit
   } catch (err) {
     alert(err);
   }
