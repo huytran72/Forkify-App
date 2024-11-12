809a4d07648121b63628ab1bab29926f5853c4d9
Huy Tran <baohuy82.th@gmail.com>
2024-11-12 15:22:24 -0800

    update

--- src/js/controller.js
+++ src/js/controller.js
@@ -115,6 +115,8 @@ const showRecipe = async function () {
       <div class="recipe__directions">
         <h2 class="heading--2">How to cook it</h2>
         <p class="recipe__directions-text">
+          This recipe was carefully designed and tested by
+          <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
             `;
             }
             )
