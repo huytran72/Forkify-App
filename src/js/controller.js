ff3a429664bd75743a2025b1374c64a275f049d8
Huy Tran <baohuy82.th@gmail.com>
2024-11-13 23:53:16 -0800

    update

--- src/js/controller.js
+++ src/js/controller.js
@@ -131,6 +131,7 @@ const showRecipe = async function () {
         </a>
       </div>
     `;
+    recipeContainer.innerHTML = '';
     recipeContainer.insertAdjacentHTML('afterbegin', markup);
   } catch (err) {
     alert(err);
