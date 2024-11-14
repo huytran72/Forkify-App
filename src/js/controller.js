147d5fa694df4f7448d0dc343f9e15be94665d87
Huy Tran <baohuy82.th@gmail.com>
2024-11-13 23:55:33 -0800

    update

--- src/js/controller.js
+++ src/js/controller.js
@@ -90,6 +90,8 @@ const showRecipe = async function () {
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
+          ${recipe.ingredients}
+
           <li class="recipe__ingredient">
             <svg class="recipe__icon">
               <use href="src/img/icons.svg#icon-check"></use>
