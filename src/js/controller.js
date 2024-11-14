1c8ed6bd0c35609dd2a1e9d6819b214a23957afb
Huy Tran <baohuy82.th@gmail.com>
2024-11-13 23:57:34 -0800

    update

--- src/js/controller.js
+++ src/js/controller.js
@@ -103,18 +103,10 @@ const showRecipe = async function () {
               </li>
             `;
           })}
-
-          <li class="recipe__ingredient">
-            <svg class="recipe__icon">
-              <use href="src/img/icons.svg#icon-check"></use>
-            </svg>
-            <div class="recipe__quantity">0.5</div>
-            <div class="recipe__description">
-              <span class="recipe__unit">cup</span> ricotta cheese
-            </div>
-          </li>
         </ul>
       </div>
+
+
 
       <div class="recipe__directions">
         <h2 class="heading--2">How to cook it</h2>
