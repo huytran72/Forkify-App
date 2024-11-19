77c325e2cbc5c80663e5e1a9e854955523806397
Huy Tran <baohuy82.th@gmail.com>
2024-11-18 23:25:27 -0800

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -3,5 +3,29 @@ export const state = {
 } 
 
 export const loadRecipe = async function(id) {
+    try {
+        const res = await fetch(
+            `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+        );
+        const data = await res.json();
 
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients,
+        }; 
+
+        state.recipe = recipe;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
