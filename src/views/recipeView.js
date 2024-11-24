64a482e53e21523a161938402a35894c3b40d67a
Huy Tran <baohuy82.th@gmail.com>
2024-11-23 23:58:43 -0800

    Update recipeView.js

--- src/views/recipeView.js
+++ src/views/recipeView.js
@@ -84,19 +84,8 @@ class RecipeView {
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
-          ${recipe.ingredients.map(ing => {
-            return `
-              <li class="recipe__ingredient">
-                <svg class="recipe__icon">
-                  <use href="src/img/icons.svg#icon-check"></use>
-                </svg>
-                <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
-                <div class="recipe__description">
-                  <span class="recipe__unit">${ing.unit}</span>${ing.description}
-                </div>
-              </li>
-            `;
-          })
+          ${recipe.ingredients
+          .map()
           .join('')}
         </ul>
       </div>
