c1ed7c87b28f7a291f9b090b657cbcfc10c6954e
Huy Tran <baohuy82.th@gmail.com>
2024-11-25 23:48:20 -0800

    Update helpers.js

--- src/views/helpers.js
+++ src/views/helpers.js
@@ -4,3 +4,4 @@ export const getJSON = async function (url) {
         const data = await res.json();
     
         if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    } catch (err) {
