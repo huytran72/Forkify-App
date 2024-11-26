ea7b07ba0eb6585f798b97eef028571b959aa6dd
Huy Tran <baohuy82.th@gmail.com>
2024-11-25 23:41:00 -0800

    Update recipeView.js

--- src/views/recipeView.js
+++ src/views/recipeView.js
@@ -119,6 +119,8 @@ class RecipeView {
           </svg>
           <div class='recipe__quantity'>${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
           <div class='recipe__description'>
+            <span class='recipe__unit'>${ing.unit}</span>
+            ${ing.description}
         `;
   }
 }
