ca07fcabd6412e61b26a063e3d29951fb44d1680
Huy Tran <baohuy82.th@gmail.com>
2024-11-26 23:59:09 -0800

    Update helpers.js

--- src/views/helpers.js
+++ src/views/helpers.js
@@ -9,6 +9,7 @@ const timeout = function (s) {
 
 export const getJSON = async function (url) {
     try {
+        const fetchPro = fetch(url);
         const res = await Promise.race[fetch(url), timeout(10)];
         const data = await res.json();
     
