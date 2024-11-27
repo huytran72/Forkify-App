540a7b09396e39ad905afa6ee5b0edc6042c9bae
Huy Tran <baohuy82.th@gmail.com>
2024-11-26 23:54:08 -0800

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -9,8 +9,7 @@ export const state = {
 
 export const loadRecipe = async function(id) {
     try {
-        const res = await fetch(
-            `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+        const res = await fetch(`${API_URL}/${id}`
         );
         const data = await res.json();
 
