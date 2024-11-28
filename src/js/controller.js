6237391eeea36e9576e5f0268a67252febd36c69
Huy Tran <baohuy82.th@gmail.com>
2024-11-27 23:07:03 -0800

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -36,19 +36,11 @@ const controlRecipes = async function () {
 
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);  
-
-    
   } catch (err) {
     alert(err);
   }
 };
-controlRecipes();
 
 
 
 
-
-['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
-
-window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
