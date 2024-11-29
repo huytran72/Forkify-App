2c5e411d014d47ed11060cbdc11405d235a977dc
Huy Tran <baohuy82.th@gmail.com>
2024-11-29 14:39:46 -0800

    Update recipeView.js

--- src/views/recipeView.js
+++ src/views/recipeView.js
@@ -46,6 +46,8 @@ class RecipeView {
         this.#parentElement.insertAdjacentHTML('afterbegin', markup);
     };  
 
+    renderMessage(message = this.#message) {
+
     addHandlerRender(handler) {
         ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
     };
