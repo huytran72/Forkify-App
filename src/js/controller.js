dcff8fd92bfc99ca60896a048f7ebe37bfbafa52
Huy Tran <baohuy82.th@gmail.com>
2024-12-01 23:40:13 -0800

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -44,7 +44,9 @@ const controlRecipes = async function () {
 const controlSearchResults = async function () {
   try {
 
-  } catch 
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const init = function () {
