12e8b9266770ac04609cfefeea0810d9368fdfaa
Huy Tran <baohuy82.th@gmail.com>
2024-12-04 23:56:27 -0800

    Update searchView.js

--- src/views/searchView.js
+++ src/views/searchView.js
@@ -2,7 +2,7 @@ class SearchView {
     #parentElement = document.querySelector('.search');
 
     getQuery() {
-
+        const query = this.#parentElement.querySelector('.search__field').value;
     }
 };
 
