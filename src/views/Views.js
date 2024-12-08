b1de6a915712a46f93cf6b02551f39980b444a03
Huy Tran <baohuy82.th@gmail.com>
2024-12-07 23:42:33 -0800

    Update Views.js

--- src/views/Views.js
+++ src/views/Views.js
@@ -4,5 +4,6 @@ export default class View {
         this._data = data;
         const markup = this._generateMarkup();
         this._clear();
+        this._parentElement.insertAdjacentHTML('afterbegin', markup);
     }
 }
