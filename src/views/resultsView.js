a60e2027c374ddfa79becd1524ead79c6efe2d2e
Huy Tran <baohuy82.th@gmail.com>
2024-12-08 23:56:15 -0800

    Update resultsView.js

--- src/views/resultsView.js
+++ src/views/resultsView.js
@@ -13,6 +13,7 @@ class ResultsView extends View {
                     <div class="preview__data">
                         <h4 class="preview__title">${this._data.title}</h4>
                         <p class="preview__publisher">${this._data.publisher}</p>
+                    </div>
         `;
     }
 }
