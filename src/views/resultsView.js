55c04a94afbfe742f4bb5a4c2a3feafa3f90bdb4
Huy Tran <baohuy82.th@gmail.com>
2024-12-08 23:54:37 -0800

    Update resultsView.js

--- src/views/resultsView.js
+++ src/views/resultsView.js
@@ -2,6 +2,10 @@ import View from "./Views";
 
 class ResultsView extends View {
     _parentElement = document.querySelector('.results');
+
+    _generateMarkup() {
+        return this._data.map(this._generateMarkupPreview).join('');
+    }
 }
 
 export default new ResultsView();
