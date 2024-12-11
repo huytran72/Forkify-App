30bd7debfc8938452404d835e381131ae1941151
Huy Tran <baohuy82.th@gmail.com>
2024-12-10 23:07:38 -0800

    Update resultsView.js

--- src/views/resultsView.js
+++ src/views/resultsView.js
@@ -6,24 +6,10 @@ class ResultsView extends View {
     _generateMarkup() {
         console.log(this._data);
         return this._data.map(this._generateMarkupPreview).join('');
-
-        return `
-            <li class="preview">
-                <a class="preview__link preview__link--active" href="#${this._data.id}">
-                    <figure class="preview__fig">
-                        <img src="${this._data.image}" alt="${this._data.title}" />
-                    </figure>
-                    <div class="preview__data">
-                        <h4 class="preview__title">${this._data.title}</h4>
-                        <p class="preview__publisher">${this._data.publisher}</p>
-                    </div>
-                </a>
-            </li>
-        `;
     }
 
     _generateMarkupPreview() {
-        
+
     }
 }
 
