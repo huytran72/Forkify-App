1905144a01eb84c92a9116cd3a6e432447b931e4
Huy Tran <baohuy82.th@gmail.com>
2024-12-10 23:13:07 -0800

    Update resultsView.js

--- src/views/resultsView.js
+++ src/views/resultsView.js
@@ -3,6 +3,7 @@ import View from "./Views";
 class ResultsView extends View {
     _parentElement = document.querySelector('.results');
     _errorMessage = 'No recipes found for your query. Please try again!';
+    _message = '';
 
     _generateMarkup() {
         console.log(this._data);
