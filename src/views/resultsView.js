cbc64db285f44e6b2d89d685933359f9c8eb2c7a
Huy Tran <baohuy82.th@gmail.com>
2024-12-10 23:08:29 -0800

    Update resultsView.js

--- src/views/resultsView.js
+++ src/views/resultsView.js
@@ -14,6 +14,7 @@ class ResultsView extends View {
                 <a class="preview__link" href="#${id}">
                     <figure class="preview__fig">
                         <img src="${image}" alt="${title}" />
+                    </figure>
         `;
     }
 }
