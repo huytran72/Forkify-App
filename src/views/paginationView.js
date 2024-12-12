1c0de6ee64cf14061d1211d9b4419093270139cb
Huy Tran <baohuy82.th@gmail.com>
2024-12-11 23:32:14 -0800

    Update paginationView.js

--- src/views/paginationView.js
+++ src/views/paginationView.js
@@ -5,6 +5,6 @@ class PaginationView extends View {
     _parentElement = document.querySelector('.pagination');
 
     _generateMarkup() {
-        
+        // Page 1, and there are other pages
     }
 }
