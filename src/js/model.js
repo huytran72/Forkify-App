f0e0f53a9d8c88bfa3386716b674c50a50a24e86
Huy Tran <baohuy82.th@gmail.com>
2024-12-11 23:28:03 -0800

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -61,4 +61,6 @@ export const loadSearchResults = async function(query) {
 
 export const getSearchResultsPage = function(page = state.search.page) { 
     state.search.page = page;
+
+    const start = (page - 1) * state.search.resultsPerPage;
 };
