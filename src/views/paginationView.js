efdbbe00f0cad21770f12fd5530714a73ac1bbf5
Huy Tran <baohuy82.th@gmail.com>
2024-12-12 13:52:19 -0800

    Update paginationView.js

--- src/views/paginationView.js
+++ src/views/paginationView.js
@@ -7,7 +7,11 @@ class PaginationView extends View {
     _generateMarkup() {
         // Page 1, and there are other pages
         const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
+
         // Page 1, and there are NO other pages
+        if(this._data.page === 1 && numPages >1) {
+
+        }
 
         // Last page
 
