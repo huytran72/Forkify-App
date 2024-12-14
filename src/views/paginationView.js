837c0b1d2da51d96f267aa0b2cb30bceebed6bd8
Huy Tran <baohuy82.th@gmail.com>
2024-12-13 23:51:49 -0800

    Update paginationView.js

--- src/views/paginationView.js
+++ src/views/paginationView.js
@@ -19,6 +19,7 @@ class PaginationView extends View {
         if(this._data.page === numPages && numPages > 1) {
             return `
                 <button data-goto="${this._data.page - 1}" class="btn--inline pagination__btn--prev">
+                    <svg class="search__icon">
             `;
         }
 
