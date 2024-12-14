cb0bac3078428de9b68aab7828a53480258ec772
Huy Tran <baohuy82.th@gmail.com>
2024-12-13 23:52:56 -0800

    Update paginationView.js

--- src/views/paginationView.js
+++ src/views/paginationView.js
@@ -5,8 +5,9 @@ class PaginationView extends View {
     _parentElement = document.querySelector('.pagination');
 
     _generateMarkup() {
-        // Page 1, and there are other pages
         const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
+
+        // Page 1, and there are other pages
 
         // Page 1, and there are NO other pages
         if(this._data.page === 1 && numPages >1) {
