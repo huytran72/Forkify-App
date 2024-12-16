f46260445a97fececf19dbb0801bec922bec3a4c
Huy Tran <baohuy82.th@gmail.com>
2024-12-15 23:38:51 -0800

    Update paginationView.js

--- src/views/paginationView.js
+++ src/views/paginationView.js
@@ -8,17 +8,6 @@ class PaginationView extends View {
         const curPage = this._data.page;
         const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
 
-        // Page 1, and there are other pages
-            if(curPage === 1 && numPages > 1) {
-                return `
-                    <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
-                        <span>Page ${curPage + 1}</span>
-                        <svg class="search__icon">
-                            <use href="${icons}#icon-arrow-right"></use>
-                        </svg>
-                    </button>
-                `;
-            }
         // Page 1, and there are NO other pages
         if(this._data.page === 1 && numPages >1) {
             return `
