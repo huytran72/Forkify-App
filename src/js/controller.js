81768b5d47e9ccfe29abbeeda7fdb6834556ae7a
Huy Tran <baohuy82.th@gmail.com>
2024-12-15 23:43:15 -0800

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -63,6 +63,8 @@ const controlSearchResults = async function () {
 const controlPagination = function (goToPage) {
   // 1) Print NEW results
   resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render NEW pagination buttons
 };
 
 const init = function () {
