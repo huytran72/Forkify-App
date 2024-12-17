80a14d634fd345a68b256423e70be5b5c69ee2b7
Huy Tran <baohuy82.th@gmail.com>
2024-12-16 23:39:05 -0800

    Update paginationView.js

--- src/views/paginationView.js
+++ src/views/paginationView.js
@@ -61,6 +61,7 @@ class PaginationView extends View {
                     <svg class="search__icon">
                         <use href="${icons}#icon-arrow-right"></use>
                     </svg>
+                </button>
             `;
         }
 
