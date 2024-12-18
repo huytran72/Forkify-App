6df6b9fcebdc73890bf9afa4135da825572f7109
Huy Tran <baohuy82.th@gmail.com>
2024-12-17 23:57:54 -0800

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -73,7 +73,7 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  // recipeView.render(model.state.recipe);
+  recipeView.render(model.state.recipe);
 };
 
 const init = function () {
