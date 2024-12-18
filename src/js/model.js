2c54b5e7434e760e16ea8d4bdd6697e04bf811ec
Huy Tran <baohuy82.th@gmail.com>
2024-12-17 23:55:49 -0800

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -67,3 +67,7 @@ export const getSearchResultsPage = function(page = state.search.page) {
 
     return state.search.results.slice(start, end);
 };
+
+export const updateServings = function(newServings) {
+    
+};
