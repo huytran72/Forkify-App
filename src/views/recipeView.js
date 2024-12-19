ac0af89856c3c4debc3a55a024eef61f535bec0b
Huy Tran <baohuy82.th@gmail.com>
2024-12-18 23:51:10 -0800

    Update recipeView.js

--- src/views/recipeView.js
+++ src/views/recipeView.js
@@ -11,7 +11,7 @@ class RecipeView extends View {
     _message = '';
 
     addHandlerRender(handler) {
-      
+        ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
     };
 
     reder(data) {
