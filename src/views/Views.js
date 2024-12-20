83e8b4dc1e1ce3a06c76f7f05c45d620e7254e2d
Huy Tran <baohuy82.th@gmail.com>
2024-12-20 01:42:52 -0800

    Update Views.js

--- src/views/Views.js
+++ src/views/Views.js
@@ -22,6 +22,7 @@ export default class View {
         const newElements = Array.from(newDOM.querySelectorAll('*'));
         const curElements = Array.from(this._parentElement.querySelectorAll('*'));
         console.log(curElements);
+        console.log(newElements);
     };
 
     _clear(){
