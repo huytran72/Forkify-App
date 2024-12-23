7f666beea9feceb9b840cfd133bbf73739d209a3
Huy Tran <baohuy82.th@gmail.com>
2024-12-23 15:11:44 -0800

    Update bookmarksView.js

--- src/views/bookmarksView.js
+++ src/views/bookmarksView.js
@@ -2,5 +2,5 @@ import View from "./Views";
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 class ResultsView extends View {
-
+    _parentElement = document.querySelector('.results');
 };
