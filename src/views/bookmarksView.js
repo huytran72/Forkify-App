3b20adbde5c6ebd649315e688d837f2ce4653980
Huy Tran <baohuy82.th@gmail.com>
2024-12-23 15:11:51 -0800

    Update bookmarksView.js

--- src/views/bookmarksView.js
+++ src/views/bookmarksView.js
@@ -3,4 +3,5 @@ import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 class ResultsView extends View {
     _parentElement = document.querySelector('.results');
+    _errorMessage = 'No recipes found for your query! Please try again.';
 };
