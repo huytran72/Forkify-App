004690a7c10727d9afbe32bfe71fcc39e389f97d
Huy Tran <baohuy82.th@gmail.com>
2024-12-24 23:34:51 -0800

    Update bookmarksView.js

--- src/views/bookmarksView.js
+++ src/views/bookmarksView.js
@@ -5,4 +5,9 @@ class ResultsView extends View {
     _parentElement = document.querySelector('.results');
     _errorMessage = 'No recipes found for your query! Please try again.';
     _message = '';
+
+    _generateMarkup() {
+        return this._data.map(this._generateMarkupPreview).join('');
+    }
 };
+
