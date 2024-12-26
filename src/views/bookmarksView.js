ca65b05a39648819c5abae5b5311a727d64fd323
Huy Tran <baohuy82.th@gmail.com>
2024-12-25 23:36:11 -0800

    Update bookmarksView.js

--- src/views/bookmarksView.js
+++ src/views/bookmarksView.js
@@ -9,21 +9,5 @@ class ResultsView extends View {
     _generateMarkup() {
         return this._data.map(this._generateMarkupPreview).join('');
     }
-
-    _generateMkarupPreview(result) {
-        return `
-            <li class="preview">    
-                <a class="preview__link preview__link--active" href="#${result.id}">
-                    <figure class="preview__fig">
-                        <img src="${result.image}" alt="${result.title}" />
-                    </figure>
-                    <div class="preview__data">
-                        <h4 class="preview__title">${result.title}</h4>
-                        <p class="preview__publisher">${result.publisher}</p>
-                    </div>
-                </a>
-            </li>
-        `;    
-    };
 };
 
