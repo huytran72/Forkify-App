c0da37cd562ce681fd95dde36812e41dee3ea4dc
Huy Tran <baohuy82.th@gmail.com>
2024-12-26 14:01:09 -0800

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -108,4 +108,5 @@ export const deleteBookmark = function(id) {
 
 const init = function() {
     const storage = localStorage.getItem('bookmarks');    
+    if(storage) state.bookmarks = JSON.parse(storage);
 };
