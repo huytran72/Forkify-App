89ec9ceb0ed95b01983c2784785463c1f2059324
Huy Tran <baohuy82.th@gmail.com>
2024-12-26 14:01:59 -0800

    Update bookmarksView.js

--- src/views/bookmarksView.js
+++ src/views/bookmarksView.js
@@ -8,10 +8,9 @@ class ResultsView extends View {
     _message = '';
 
     _generateMarkup() {
-        console.log(this._data);
         return this._data
-            .map(bookmark => previewView.render(bookmark, false))
-            .join('');
+        .map(bookmark => previewView.render(bookmark, false))
+        .join('');
     }
 };
 
