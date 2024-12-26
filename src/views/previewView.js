891572f29a5497286bcedacc625cd42a66e9ec5a
Huy Tran <baohuy82.th@gmail.com>
2024-12-25 23:32:00 -0800

    Update previewView.js

--- src/views/previewView.js
+++ src/views/previewView.js
@@ -5,4 +5,8 @@ class PreviewView extends View {
     _parentElement = '';    
     _errorMessage = 'No recipes found for your query. Please try again!';
     _message = '';
+
+    _generateMarkup() {
+        return this._data.map(this._generateMarkupPreview).join('');
+    }
 };
