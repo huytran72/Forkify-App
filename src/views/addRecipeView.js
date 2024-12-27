09e01692d9be91c665dbace98f5b6c88a6b0d3d7
Huy Tran <baohuy82.th@gmail.com>
2024-12-27 12:45:42 -0800

    Update addRecipeView.js

--- src/views/addRecipeView.js
+++ src/views/addRecipeView.js
@@ -3,6 +3,8 @@ import icons from "url:../../img/icons.svg"; // Parcel 2
 
 class AddRecipeView extends View {
     _parentElement = document.querySelector(".upload"); 
+
+    _message = "Recipe was successfully uploaded";
     
     _generateMarkup() {}
 };
