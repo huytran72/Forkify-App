e61e6ad1fe6532f649ca79a13beaad410c8de5ee
Huy Tran <baohuy82.th@gmail.com>
2024-12-27 12:47:10 -0800

    Update addRecipeView.js

--- src/views/addRecipeView.js
+++ src/views/addRecipeView.js
@@ -8,6 +8,7 @@ class AddRecipeView extends View {
 
     _window = document.querySelector(".add-recipe-window");
     _overlay = document.querySelector(".overlay");
+    _btnOpen = document.querySelector(".nav__btn--add-recipe");
     
     _generateMarkup() {}
 };
