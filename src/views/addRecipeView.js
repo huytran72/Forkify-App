230348c07e49a662a096979c1720e08fc24bcb04
Huy Tran <baohuy82.th@gmail.com>
2024-12-28 23:53:04 -0800

    Update addRecipeView.js

--- src/views/addRecipeView.js
+++ src/views/addRecipeView.js
@@ -33,6 +33,8 @@ class AddRecipeView extends View {
 
     addHandlerUpload(handler) {
         this._parentElement.addEventListener("submit", function(e) {    
+            
+        });
     };
     
     _generateMarkup() {}
