f51a6be0785ac3bbea1131c5b7a6d30d05fa3f39
Huy Tran <baohuy82.th@gmail.com>
2024-12-28 23:52:10 -0800

    Update addRecipeView.js

--- src/views/addRecipeView.js
+++ src/views/addRecipeView.js
@@ -28,6 +28,7 @@ class AddRecipeView extends View {
 
     _addHandlerHideWindow() {
         this._btnClose.addEventListener("click", this.toggleWindow);    
+        this._overlay.addEventListener("click", this.toggleWindow);
     };
     
     _generateMarkup() {}
