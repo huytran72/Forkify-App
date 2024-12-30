e79c213b1323eec0ef400eddcfa298c6b5502b3a
Huy Tran <baohuy82.th@gmail.com>
2024-12-29 23:54:50 -0800

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -138,6 +138,7 @@ export const uploadRecipe = async function(newRecipe) {
         image_url: newRecipe.image,
         publisher: newRecipe.publisher,
         cooking_time: +newRecipe.cookingTime,
+        servings: +newRecipe.servings,
     };
 
 
