6cfd4f0afb7540d0bbad87a7d3bdd92a1f500404
Huy Tran <baohuy82.th@gmail.com>
2024-12-31 10:07:12 -0800

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -112,6 +112,7 @@ const controlAddRecipe = async function (newRecipe) {
     // Render bookmark view
     bookmarksView.render(model.state.bookmarks);
 
+    // Change ID in URL
     window.history.pushState(null, '', `#${model.state.recipe.id}`);
 
     // Close form window
