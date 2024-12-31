866e95b499532e1ecbc9bf8ee6cdca14a83eb65e
Huy Tran <baohuy82.th@gmail.com>
2024-12-31 10:04:38 -0800

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = 'f1b3b3b4-0b3b-4b3b-8b3b-3b3b3b3b3b3b';
+export const MODAL_CLOSE_SEC = 2.5;
