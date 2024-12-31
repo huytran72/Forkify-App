34915ab7ca21464f4954b9cb82403a0d1c870526
Huy Tran <baohuy82.th@gmail.com>
2024-12-30 22:48:03 -0800

    Update helpers.js

--- src/views/helpers.js
+++ src/views/helpers.js
@@ -27,6 +27,12 @@ export const getJSON = async function (url) {
 export const sendJSON = async function (url, uploadData) {
   try {
     const fetchPro = fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(uploadData),
+    });
   } catch () {  
 
   }  
