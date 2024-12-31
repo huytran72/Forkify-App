cca0b0419b727ce527e6009da0b83e92490dcebe
Huy Tran <baohuy82.th@gmail.com>
2024-12-30 22:49:34 -0800

    Update helpers.js

--- src/views/helpers.js
+++ src/views/helpers.js
@@ -35,6 +35,7 @@ export const sendJSON = async function (url, uploadData) {
     });
 
     const res = await Promise.race(fetchPro, timeout(TIMEOUT_SEC));
+    const data = await res.json();
   } catch (err) {  
     console.error(err);
     throw err;
